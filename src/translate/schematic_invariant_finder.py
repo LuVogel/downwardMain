b9e283fe1ff03ab2c12414befee2f75fb44e0c2c
LuVogel <vogel.lukas@hotmail.com>
2023-04-29 16:32:26 +0200

    add notes and until sat solver

--- src/translate/schematic_invariant_finder.py
+++ src/translate/schematic_invariant_finder.py
@@ -1,13 +1,6 @@
 from src.translate.pddl.effects import *
 from pddl.conditions import *
 
-
-#Besprechung: del und add effects --> del effects einfach negieren
-# rintannen: schmematische vs gegroundete invarianten.
-# invarianten: Kernaussagen von Blocksworld --> nicht für spezifisches problem
-# regression test mit Action die Conditional Effect hat
-
-
 # ist keine action sondern state-variable --> oder doch effects? 2 Definitions 1. a and not a for state variables a in A are effects?
 def negate_state_var(operator):
     return operator
@@ -29,7 +22,7 @@ def regression_rec(formula, effect):
     if isinstance(formula, Falsity):
         return Falsity()
     if isinstance(formula, NegatedAtom):
-        return regression_rec(formula, effect).negate()
+        return regression_rec(formula.negate(), effect).negate()
     if isinstance(formula, Disjunction) or isinstance(formula, Conjunction):
         regr_list = []
         for part in formula.parts:
@@ -81,20 +74,6 @@ def regression(formula, operator):
 # for instance in c = c\sigma
 # visitor pattern s
 
-
-
-# TODO: invarianten:
-# Eine grosse Conjunction von diesen Punkten oder eine Liste von einzelnen Formulas für C?
-# 1. Anzahl d. Blöcke ändern sich nicht während eines Problems
-# 2. Jeder Block liegt entweder auf dem Tisch oder auf einem anderen Block --> oder auch in der Hand? dann nicht zu beginn gültig
-#          könnte in dieser art gemacht werden: for all i \in {1, ..., N} für N Blöcke: onTable(i) or (exist j != i mit on(i,j))
-# 3. ein Block kann sich nur bewegen, wenn er frei ist
-# 4. es gibt eine begrenze Anzahl an Aktionen die ausgeführt werden können: zu Beginn alle möglichen Actions sammeln
-# 5. Regeln wie es is nicht möglich Blöcke zu zerstören oder in der Luft zu schweben??
-# 6. es ist nicht möglich gleichzeitig mehrere Blöcke zu bewegen oder zu stapeln
-# 7. es gibt eine bestimmte Anzahl an positionen wo sich ein Block befinden kann
-# 8. jede Block ist eindeutig --> name == id?
-
 # bei grosse Conjunction: for parts in Conjunction.parts --> regr(not parts, action)
 # bei Liste: for form in list: --> regr(not form, action)
 def is_sat(temp_union):
@@ -134,7 +113,18 @@ def create_invariant_candidates(task):
                     if obj != obj1:
                         inv_list.append(Atom(predicate=name, args=[obj1, obj]))
                         inv_list.append(Atom(predicate=name, args=[obj, obj1]))
-    print(inv_list)
+    return inv_list
+
+
+def create_union(C_0, x):
+    x_list = []
+    for part in x.parts:
+        x_list.append(part)
+    union_list = list(set(C_0 + x_list))
+    return Conjunction(union_list)
+
+
+
 def get_schematic_invariants(relaxed_reachable, atoms, actions, goal_list, axioms,
                              reachable_action_params, task):
     print("relaxed reachable: ", relaxed_reachable)
@@ -148,17 +138,17 @@ def get_schematic_invariants(relaxed_reachable, atoms, actions, goal_list, axiom
     for pred in task.predicates:
         print(pred)
     print("task_objects: ", task.objects)
-    invariant_candidates = create_invariant_candidates(task)
+    invariant_candidates = list(set(create_invariant_candidates(task)))
+    #print(invariant_candidates)
     #invariant candidates:
-    # [<Atom on(d, ?x)>, <Atom on(?x, d)>, <Atom on(d, ?y)>, <Atom on(?y, d)>, <Atom on(b, d)>, <Atom on(d, b)>, <Atom on(a, d)>, <Atom on(d, a)>, <Atom on(c, d)>, <Atom on(d, c)>, <Atom on(b, ?x)>, <Atom on(?x, b)>, <Atom on(b, ?y)>, <Atom on(?y, b)>, <Atom on(d, b)>, <Atom on(b, d)>, <Atom on(a, b)>, <Atom on(b, a)>, <Atom on(c, b)>, <Atom on(b, c)>, <Atom on(a, ?x)>, <Atom on(?x, a)>, <Atom on(a, ?y)>, <Atom on(?y, a)>, <Atom on(d, a)>, <Atom on(a, d)>, <Atom on(b, a)>, <Atom on(a, b)>, <Atom on(c, a)>, <Atom on(a, c)>, <Atom on(c, ?x)>, <Atom on(?x, c)>, <Atom on(c, ?y)>, <Atom on(?y, c)>, <Atom on(d, c)>, <Atom on(c, d)>, <Atom on(b, c)>, <Atom on(c, b)>, <Atom on(a, c)>, <Atom on(c, a)>, <Atom ontable(?x)>, <Atom ontable(d)>, <Atom ontable(b)>, <Atom ontable(a)>, <Atom ontable(c)>, <Atom clear(?x)>, <Atom clear(d)>, <Atom clear(b)>, <Atom clear(a)>, <Atom clear(c)>, <Atom handempty()>, <Atom holding(?x)>, <Atom holding(d)>, <Atom holding(b)>, <Atom holding(a)>, <Atom holding(c)>]
-    task_init = task.init
-    task_predicates = task.predicates
-    list_of_true_in_init = []
-    for i in task_init:
-        if i.predicate != "=":
-            list_of_true_in_init.append(i)
-    # TODO: im moment werden nur init-Werte als Conjunction genommen. Also nur formulas welche true in initial state sind
-    C = Conjunction(list_of_true_in_init)
+    # [<Atom on(b, ?x)>, <Atom holding(d)>, <Atom on(?y, b)>, <Atom on(d, ?y)>, <Atom on(a, ?y)>, <Atom clear(d)>,
+    # <Atom ontable(b)>, <Atom ontable(?x)>, <Atom on(d, a)>, <Atom holding(c)>, <Atom on(b, d)>, <Atom clear(c)>,
+    # <Atom handempty()>, <Atom on(?y, d)>, <Atom on(b, c)>, <Atom ontable(d)>, <Atom on(?x, a)>, <Atom on(c, ?y)>,
+    # <Atom on(?y, c)>, <Atom on(c, a)>, <Atom holding(a)>, <Atom on(a, ?x)>, <Atom on(c, ?x)>, <Atom on(d, b)>,
+    # <Atom on(d, ?x)>, <Atom clear(a)>, <Atom on(b, ?y)>, <Atom ontable(c)>, <Atom on(?x, d)>, <Atom on(?x, b)>,
+    # <Atom on(?y, a)>, <Atom on(a, d)>, <Atom on(c, d)>, <Atom on(a, b)>, <Atom on(c, b)>, <Atom holding(b)>,
+    # <Atom holding(?x)>, <Atom on(?x, c)>, <Atom ontable(a)>, <Atom clear(b)>, <Atom on(a, c)>, <Atom clear(?x)>,
+    # <Atom on(d, c)>, <Atom on(b, a)>]
     temp = 0
     action_temp = None
     # contains all possible actions (not depending on inital state)
@@ -171,27 +161,34 @@ def get_schematic_invariants(relaxed_reachable, atoms, actions, goal_list, axiom
                 list_of_possible_actions.append(a)
         else:
             list_of_possible_actions.append(a)
-        if temp == 11:
+        if temp == 0:
             action_temp = a
-        temp += 1
+            temp = 1
+    #     if temp == 11:
+    #         action_temp = a
+    #     temp += 1
     a = Atom(predicate="on", args=["a", "d"])
     b = Atom(predicate="on", args=["d", "c"])
 
     #print(list_of_possible_actions)
 
-    conj = Conjunction([a, b])
-    z = regression(a, action_temp).simplified()
+    #conj = Conjunction([a, b])
+    z = regression(b.negate(), action_temp).simplified()
     print("after regression: ")
     z.dump()
 
     while True:
-        C_0 = C
-        # TODO: unstack(a,d) in actions drin? wenn init alle ontable()?
+        C_0 = invariant_candidates
         for action in list_of_possible_actions:
             # TODO: \lnot c\sigma --> im moment wird nur C.part[i].negate() genommen
-            for c in C.parts:
-                x = regression(c.negate(), action)
-                temp_union = set([C_0, x])
+            for c in invariant_candidates:
+                #print("starting regression with: ", c.negate(), "and action: ", action)
+                x = regression(c.negate(), action).simplified()
+                # print("after regression in while loop")
+                # x.dump()
+                temp_union = create_union(C_0, x)
+                # print("temp_union: ")
+                # temp_union.dump()
                 # TODO: is_sat --> im moment wird True zurück gegeben
                 # könnte z.b mit pycosat gemacht werden (sat-solver), dann muss jedoch formula umgeformt werden
                 # eigenen sat solver implementieren
