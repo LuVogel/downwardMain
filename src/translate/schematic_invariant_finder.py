d83dadf5ba17ac5de5cfd804c44314f4eac83dc0
LuVogel <vogel.lukas@hotmail.com>
2023-05-26 12:09:45 +0200

    Besprechung 26.5.

--- src/translate/schematic_invariant_finder.py
+++ src/translate/schematic_invariant_finder.py
@@ -27,34 +27,34 @@ def weaken(formula, objects):
                 cond_obj2 = random.choice(objects).name
             return Disjunction(parts=[formula, Atom(predicate="on", args=[cond_obj1, cond_obj2])])
     # weaken of form X -> l_1 v ... v l_n to X -> l_1 v .. v l_n v l_n+1
-    elif isinstance(formula, Disjunction):
-        list = []
-        for part in formula.parts:
-            list.append(part)
-        # start with a condition (Atom) which is in formula
-        cond = list[0]
-        # as long as condition is in list
-        while cond in list:
-            # choose random predicate
-            cond_pred = random.choice(["on", "handempty", "ontable", "clear", "holding"])
-            # if on was choosen, chose two random objects (on(x,y)) with x != y and set this to cond
-            if cond_pred == "on":
-                cond_obj1 = random.choice(objects).name
-                cond_obj2 = random.choice(objects).name
-                while cond_obj2 == cond_obj1:
-                    cond_obj2 = random.choice(objects).name
-                cond = Atom(predicate=cond_pred, args=[cond_obj1, cond_obj2])
-            # if handempty, create new atom handempty
-            elif cond_pred == "handempty":
-                cond = Atom(predicate=cond_pred, args=["#"])
-            else:
-                # in other cases, create cond with random object
-                cond_obj = random.choice(objects).name
-                cond = Atom(predicate=cond_pred, args=[cond_obj])
-        # if we land here, that means we created an new Atom which wasn't already in the formula.
-        # therefore add it to list and return a disjunction (formula = x1 v...v xn) return x1 v...v xn v cond
-        list.append(cond)
-        return Disjunction(list)
+    # elif isinstance(formula, Disjunction):
+    #     list = []
+    #     for part in formula.parts:
+    #         list.append(part)
+    #     # start with a condition (Atom) which is in formula
+    #     cond = list[0]
+    #     # as long as condition is in list
+    #     while cond in list:
+    #         # choose random predicate
+    #         cond_pred = random.choice(["on", "handempty", "ontable", "clear", "holding"])
+    #         # if on was choosen, chose two random objects (on(x,y)) with x != y and set this to cond
+    #         if cond_pred == "on":
+    #             cond_obj1 = random.choice(objects).name
+    #             cond_obj2 = random.choice(objects).name
+    #             while cond_obj2 == cond_obj1:
+    #                 cond_obj2 = random.choice(objects).name
+    #             cond = Atom(predicate=cond_pred, args=[cond_obj1, cond_obj2])
+    #         # if handempty, create new atom handempty
+    #         elif cond_pred == "handempty":
+    #             cond = Atom(predicate=cond_pred, args=["noargs"])
+    #         else:
+    #             # in other cases, create cond with random object
+    #             cond_obj = random.choice(objects).name
+    #             cond = Atom(predicate=cond_pred, args=[cond_obj])
+    #     # if we land here, that means we created an new Atom which wasn't already in the formula.
+    #     # therefore add it to list and return a disjunction (formula = x1 v...v xn) return x1 v...v xn v cond
+    #     list.append(cond)
+    #     return Disjunction(list)
 
 def regression(formula, operator):
     # eff_list = eff(o)
@@ -123,7 +123,7 @@ def formula_to_list(formula):
         for arg in part.args:
             # handempty: fof file don't recognize empty brackets
             if arg == " ":
-                arg = "#"
+                arg = "noargs"
             elif arg == "?x":
                 arg = "X"
                 x_found = True
@@ -165,13 +165,13 @@ def write_formula_to_fof(formula, type, file, counter):
             file.write("![Y]:")
         file.write(" | ".join(list_formula) + ").\n")
     elif isinstance(formula, Atom) or isinstance(formula, NegatedAtom):
-        # atom does not need formula converion
+        # atom does not need formula conversion
         part_args = ""
         x_found = False
         y_found = False
         for arg in formula.args:
             if arg == " ":
-                arg = "#"
+                arg = "noargs"
             elif arg == "?x":
                 arg = "X"
                 x_found = True
@@ -208,20 +208,20 @@ def is_sat(negated_conjecture, axiom_list):
     # run vampire
     result = subprocess.run(['vampire', 'src/translate/tptp-formulas.p'], capture_output=True)
     # return 0 for Satisfiable, else Refutation
-    return result.returncode
+    return result.returncode == 0
 
 def handempty_conversion(x):
-    # since vampire doesn't recognize empty claues, use "#" for handempty()
+    # since vampire doesn't recognize empty claues, use "noargs" for handempty()
     if isinstance(x, Conjunction) or isinstance(x, Disjunction):
         for part in x.parts:
             if isinstance(part, Conjunction) or isinstance(part, Conjunction):
                 return handempty_conversion(part)
             if isinstance(part, Atom) or isinstance(part, NegatedAtom):
                 if part.predicate == "handempty":
-                    part.args = ["#"]
+                    part.args = ["noargs"]
     if isinstance(x, Atom) or isinstance(x, NegatedAtom):
         if x.predicate == "handempty":
-            x.args = ["#"]
+            x.args = ["noargs"]
     return x
 
 
@@ -231,7 +231,7 @@ def create_invariant_candidates(task):
     for a in task.init:
         if a.predicate != "=":
             if a.predicate == "handempty":
-                a.args = ["#"]
+                a.args = ["noargs"]
             inv_list.append(a)
     return inv_list
 
@@ -249,20 +249,45 @@ def get_schematic_invariants(task, actions):
             y = x[2].split(")")
             if x[1] != y[0]:
                 list_of_possible_actions.append(a)
+
         else:
             list_of_possible_actions.append(a)
-
     # start algorithm from Rintannen
     while True:
-        C_0 = C
+        C_0 = list(C)
+        print("C")
+        for part in C_0:
+            part.dump()
+        print("end c ")
         for action in list_of_possible_actions:
+            # kommt in der aktion ein negiertes literal vor welches in c enthalten ist --> kann action überhaupt effekt auf candidates haben
+            # if else check
+            print("action:")
+            action.dump()
             for c in C:
+                # hier alle möglichen c instanziierungen (mit unterschiedlichen objekten testen) --> nur wenn mit action ungültig gemacht werden
+                #
+                print("c")
+                c.dump()
+
                 after_reg = regression(c.negate(), action).simplified()
+                print("after reg:")
+                after_reg.dump()
                 after_reg_and_conv = handempty_conversion(after_reg)
+
                 sat_test = is_sat(after_reg_and_conv, C_0)
-                if sat_test == 0:
+                if sat_test:
+                    print("no invariant")
+                    print("c length: ", len(C))
                     C.remove(c)
+                    print("c length: ", len(C))
+                    # aktion übergeben zu sat test
+                    # schwächt schematische invarianten ab
+                    # hier muss geprüft werden ob C wächst, falls nicht --> emptyObject oder so übergeben (da C sonst grösse ändert innerhalb iteration)
                     C.add(weaken(c, task.objects))
+                    print("c length: ", len(C))
+                else:
+                    print("invariant")
         if C == C_0:
             # solution found, return
             return C
--- src/translate/tptp-formulas.p
+++ src/translate/tptp-formulas.p
@@ -1,10 +1,10 @@
-fof(formula1, axiom,clear(a)).
-fof(formula2, axiom,ontable(a)).
+fof(formula1, axiom,clear(b)).
+fof(formula2, axiom,clear(a)).
 fof(formula3, axiom,clear(d)).
-fof(formula4, axiom,ontable(c)).
-fof(formula5, axiom,clear(c)).
-fof(formula6, axiom,ontable(b)).
-fof(formula7, axiom,clear(b)).
-fof(formula8, axiom,ontable(d)).
-fof(formula9, axiom,handempty(#)).
-fof(formula0, negated_conjecture,on(d,c) & clear(d) & handempty(#) & handempty(#)).
+fof(formula4, axiom,ontable(a)).
+fof(formula5, axiom,ontable(b)).
+fof(formula6, axiom,handempty(noargs)).
+fof(formula7, axiom,ontable(d)).
+fof(formula8, axiom,clear(c)).
+fof(formula9, axiom,ontable(c)).
+fof(formula0, negated_conjecture,clear(a) & ontable(a) & handempty(noargs) & ontable(d) & ontable(b)).
